58987c3f77a9ac565ead714a5a9054b95357fced
Danil22May <daniilmej88@gmail.com>
2024-05-23 09:42:03 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const pasosPreparacionPizza = [
+    "Se disuelve la levadura seca y el azúcar en el agua tibia. Después de 10 minutos, la mezcla ya está espumosa y lista para usar.",
+    "En un bol grande, se mezclan la harina de trigo y la sal.",
+    "Ahora se añade la mezcla de levadura y el aceite de oliva a la harina. La masa comienza a formarse.",
+    "La masa se está amasando sobre una superficie ligeramente enharinada. Después de 10 minutos, ya está suave y elástica.",
+    "La masa se coloca en un bol ligeramente engrasado con aceite de oliva. Cubierta con un paño húmedo, se deja reposar en un lugar cálido. En 1-2 horas, la masa ha duplicado su tamaño.",
+    "Después del primer levado, la masa se golpea para quitarle el aire. Luego, se divide (si se desea) y se estira sobre una superficie enharinada hasta obtener el grosor deseado.",
+    "El horno ya está precalentado a 220°C (428°F).",
+    "Se extiende una capa uniforme de salsa de tomate sobre la masa estirada.",
+    "Se espolvorean 200 gramos de queso mozzarella rallado sobre la salsa de tomate.",
+    "Se colocan 100 gramos de pepperoni en rodajas de manera uniforme sobre el queso.",
+    "Se añaden 20 aceitunas negras o verdes sobre el pepperoni.",
+    "Se colocan 4 rodajas de queso de cabra sobre la pizza.",
+    "Se añade sal, orégano, albahaca y otras especias al gusto para darle más sabor.",
+    "La pizza se coloca en el horno precalentado y se hornea durante 15-20 minutos. La masa está dorada y el queso burbujeante y ligeramente dorado.",
+    "La pizza se saca del horno y se deja enfriar unos minutos antes de cortarla y servirla. ¡Disfrutando de la deliciosa pizza casera!"
+];
+function prepararPizza(){
+    pasosPreparacionPizza.forEach((paso) =>{
+        console.log(paso);
+    })
+}
+
+prepararPizza();
